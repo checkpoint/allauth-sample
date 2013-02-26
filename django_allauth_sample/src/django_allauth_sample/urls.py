c152992155501d9117e51a3492808c5511d1bbc0
Hironori Sekine <checkpoint777@gmail.com>
2013-02-26 12:32:37 +0900

    project add

---
+++ django_allauth_sample/src/django_allauth_sample/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib.auth.decorators import login_required
+from django.views.generic.simple import direct_to_template
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_allauth_sample.views.home', name='home'),
+    # url(r'^django_allauth_sample/', include('django_allauth_sample.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^$', login_required(direct_to_template), {'template': 'index.html'}),
+    )
